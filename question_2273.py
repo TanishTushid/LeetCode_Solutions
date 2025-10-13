d800a23a124e75b9a8c36ffaa207f21f0140cb95
Tanish Tushid <tanish.tushid.2004@gmail.com>
2025-10-13 21:08:26 +0530

    Add files via upload

---
+++ question_2273.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def removeAnagrams(self, words):
+        res = [words[0]]
+        for word in words[1:]:
+            if sorted(word) != sorted(res[-1]):
+                res.append(word)
+        return res
+        
