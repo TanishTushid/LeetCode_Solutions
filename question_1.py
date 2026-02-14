ab435b02a2d6dee56f1a63219ec487e6b0b64bff
Tanish Tushid <tanish.tushid.2004@gmail.com>
2026-02-14 12:59:20 +0530

    Add twoSum method for finding two indices
    Implement twoSum method to find indices of two numbers that add up to target.

---
+++ question_1.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        #brute
+        """ 
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)):
+                if nums[i] + nums[j] == target:
+                    return i, j
+        """
+        #optimise
+        hashmap = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+
+            if complement in hashmap:
+                return [hashmap[complement], i ]   
+            hashmap[num] = i       
